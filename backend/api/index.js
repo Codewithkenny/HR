e6c91a46f3ffc4389ed9bd39e82ecd0596359b2f
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2024-08-01 16:16:10 +0100

    removed app.listen

--- backend/api/index.js
+++ backend/api/index.js
@@ -44,10 +44,10 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
 
 
 module.exports = app;

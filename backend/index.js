ab55bfc0cc40c7a4a01a82d7b23b7129b5557f99
Olapoju Kenny Agbomeji <pojuagbomeji@gmail.com>
2024-08-01 10:17:24 +0100

    updated the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
 const employeeRoutes = require("./routes/employeeRoutes");
 const activityRoutes = require("./routes/activityRoutes");
 const departmentRoutes = require("./routes/departmentRoutes");
@@ -10,25 +11,24 @@ const announcementRoutes = require("./routes/announcementRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const supportRoutes = require("./routes/supportRoutes");
 const profileRoutes = require("./routes/profileRoutes");
-const app = express();
-const PORT = process.env.PORT || 5000;
 
+const app = express();
+
+// Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow requests from this origin
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-// Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Welcome to Our Backend Services !");
+  res.send("Welcome to Our Backend Services!");
 });
 
 app.use("/api/employees", employeeRoutes);
@@ -45,7 +45,5 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
